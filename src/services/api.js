182f07ba8f548720a785c5a1af7ab74637ff84d3
victor <vdbetancurgo@cesde.net>
2026-03-18 08:28:18 -0500

    (Chore):Definicion de endpoints y url basede la api para proxima comunicacion de back

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+const URL_BASE = "localhost:8080/"
+
+export let end_point = {
+    users: URL_BASE + "users/",
+    merchants: URL_BASE + "merchanst/",
+    expenses: URL_BASE + "expenses/",
+    payment_methods: URL_BASE + "payment_methods/",
+    category: URL_BASE + "category/"
+
+}
